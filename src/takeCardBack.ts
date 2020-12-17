ea380e62e0b4489da0fb399d7f2cb1dafa4c8a45
Cornelius Müller <muellercornelius1@gmail.com>
2020-12-17 22:32:00 +0100

    fixed card duplicate bug

--- src/takeCardBack.ts
+++ src/takeCardBack.ts
@@ -1,13 +1,18 @@
 import { Card, DurakState } from "./rooms/schema/durakState";
 
 export const takeCardBack = (state: DurakState, data: any) => {
-    let player = state.players.get(data.card.owner)
-    player.cards.push(new Card(data.card))
+  let player = state.players.get(data.card.owner);
+  const filtered = player.cards.filter((card: Card) => card.color == data.card.color && card.number == data.card.number)
+  if (filtered.length < 1) {
+    player.cards.push(new Card(data.card));
+  }
 
-    let tableCards = JSON.parse(state.tableCards)
-    tableCards.forEach((cards:any,i:any) => {
-        tableCards[i] = tableCards[i].filter((card: Card) => card.color !== data.card.color || card.number !== data.card.number)
-    })
-    state.tableCards = JSON.stringify(tableCards)
+  let tableCards = JSON.parse(state.tableCards);
+  tableCards.forEach((cards: any, i: any) => {
+    tableCards[i] = tableCards[i].filter(
+      (card: Card) =>
+        card.color !== data.card.color || card.number !== data.card.number
+    );
+  });
+  state.tableCards = JSON.stringify(tableCards);
 };
-
